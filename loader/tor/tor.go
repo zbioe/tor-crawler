389ef9b4187dbc3983bba40ad088480d1c014fd2
Iury Fukuda <iuryfukuda@gmail.com>
2018-05-22 17:29:04 -0300

    fix tor proxy

--- loader/tor/tor.go
+++ loader/tor/tor.go
@@ -2,15 +2,13 @@ package tor
 
 import (
 	"fmt"
-	"net/url"
 	"net/http"
 	"golang.org/x/net/proxy"
 )
 
 const (
-	ENDPOINT = "socks5://127.0.0.1:9050"
+	ENDPOINT = "127.0.0.1:9050"
 	EDIALERPROXY = "Failed to create new dialer"
-	EPARSEPROXY  = "Failed to obtain new proxy"
 )
 
 func errFmt(s string, err error) error {
@@ -18,16 +16,11 @@ func errFmt(s string, err error) error {
 }
 
 func New() (*http.Client, error) {
-	u, err := url.Parse(ENDPOINT)
-	if err != nil {
-		return &http.Client{}, errFmt(EPARSEPROXY, err)
-	}
-
-	dialer, err := proxy.FromURL(u, proxy.Direct)
+	dialer, err := proxy.SOCKS5("tcp", ENDPOINT, nil, proxy.Direct)
 	if err != nil {
 		return &http.Client{}, errFmt(EDIALERPROXY, err)
 	}
-	
+
 	return &http.Client{
 		Transport: &http.Transport{
 			Dial: dialer.Dial,
